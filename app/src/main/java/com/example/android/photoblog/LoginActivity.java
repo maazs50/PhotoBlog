690809dd931602d6791e0d9e7bfbd4c0b38c569c
Mohammed-Maaz <34455354+Mohammed-Maaz@users.noreply.github.com>
2018-04-27 09:11:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/photoblog/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.example.android.photoblog;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private EditText loginEmailText;
+    private EditText loginPassText;
+    private Button loginBtn;
+    private Button loginRegBtn;
+    private ProgressBar  loginProgress;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+        loginEmailText = findViewById(R.id.reg_email);
+        loginPassText = findViewById(R.id.reg_confirm_pass);
+        loginBtn = findViewById(R.id.login_btn);
+        loginRegBtn = findViewById(R.id.login_reg_btn);
+        loginProgress=findViewById(R.id.login_progress);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String loginEmail=loginEmailText.getText().toString();
+                String loginPass =loginPassText.getText().toString();
+                if (!TextUtils.isEmpty(loginEmail)&&!TextUtils.isEmpty(loginPass)){
+                    loginProgress.setVisibility(View.VISIBLE);
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPass)
+                            .addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+
+                                     sendToMain();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        String errorMessage=task.getException().toString();
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                    loginProgress.setVisibility(View.INVISIBLE);
+                                    // ...
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser=mAuth.getCurrentUser();
+        if (currentUser!=null){
+            sendToMain();
+        }
+    }
+   public void sendToMain(){
+        Intent mainIntent=new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
+}
